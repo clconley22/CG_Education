3acba89e59395e17a20a97ea9f4d158b380fa26c
Christina Conley <clconley22@gmail.com>
2018-12-12 16:07:17 -0600

    finished part 1

---
+++ js/main.js
@@ -0,0 +1,30 @@
+console.log('this is linked')
+//Teacher data
+let teacherName = "Matthew Riffee";
+let departmentName = "Science";
+let ratingOne = 3.4;
+let ratingTwo = 5.0;
+let ratingThree = 4.0;
+
+console.log('Teacher: ' + teacherName);
+console.log('Department: ' + departmentName);
+console.log('Ratings: ' + ratingOne + ', ' + ratingTwo.toFixed(1) + ', ' + ratingThree.toFixed(1));
+
+const avgRating = ((ratingOne + ratingTwo + ratingThree)/3);
+
+console.log('Average Rating: ' + avgRating.toFixed(5));
+
+//Student Data
+
+let studentName = "Christina Conley";
+let studentMajor = 'Business';
+let studentEmail = 'clconley22@gmail.com';
+let studentGPA = 3.5;
+
+console.log('Student: ' + studentName);
+console.log('Major: ' + studentMajor);
+console.log('Email: ' + studentEmail);
+console.log('GPA: ' + studentGPA);
+
+//Course data
+let courseName: "Biology";
